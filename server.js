0d4c257aca3fb448096be8021b09dc040c3cf45a
Niladri Basak <niladri.basak.007@gmail.com>
2024-07-07 23:59:31 +0530

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const File = require("./models/File");
 const express = require("express");
+const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static("public"));
+app.use("/public", express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 const s3Client = new S3Client({
